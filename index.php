a06163b2fe1f4c31baa1bcaa4b567d950fc99f36
FU51ON <33075062+FU51ON@users.noreply.github.com>
2019-08-07 16:58:12 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    include "includes/dbh.php";
+    /*echo password_hash("rasmuslerdorf", PASSWORD_DEFAULT);*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<a href="includes/register.php">Register</a><br>
+<a href="includes/login.php">Login</a>
+</body>
+</html>
